29a8ee4dd3eb5643955cdbc861da32c068654fec
Ankush115 <alwagh.45@gmail.com>
2026-05-05 18:19:01 +0530

    projects details updated

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
+        <Header  />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects/>} />
