0f2d070047180f94d1bf24c82cdb57fb94c9963f
Ankush115 <alwagh.45@gmail.com>
2026-05-07 18:53:53 +0530

    fixed some issue

--- my-portfolio/src/pages/ProjectsPage.jsx
+++ my-portfolio/src/pages/ProjectsPage.jsx
@@ -4,7 +4,6 @@ import Projects from "../components/Projects";
 export default function ProjectsPage() {
   return (
     <main>
-      <p>Showcasing my best work and professional experience</p>
       <Projects />
     </main>
   );
